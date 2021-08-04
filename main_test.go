dc5f7b48f4520b353b2d558e7c999be85890060f
leejoys <user@example.local>
2021-08-04 07:40:09 +0300

    ecies bug

--- main_test.go
+++ main_test.go
@@ -14,25 +14,45 @@ import (
 
 var result []byte
 
-func Benchmark_RSA2048_47(b *testing.B) { RSA(2048, []byte(generateString(47)), b) }
+func Benchmark_RSA2048_47(b *testing.B) {
+	RSA(2048, []byte(generateString(47)), b)
+}
 
-func Benchmark_RSA2048_94(b *testing.B) { RSA(2048, []byte(generateString(94)), b) }
+func Benchmark_RSA2048_94(b *testing.B) {
+	RSA(2048, []byte(generateString(94)), b)
+}
 
-func Benchmark_RSA2048_188(b *testing.B) { RSA(2048, []byte(generateString(188)), b) }
+func Benchmark_RSA2048_188(b *testing.B) {
+	RSA(2048, []byte(generateString(188)), b)
+}
 
-func Benchmark_RSA4096_47(b *testing.B) { RSA(4096, []byte(generateString(47)), b) }
+func Benchmark_RSA4096_47(b *testing.B) {
+	RSA(4096, []byte(generateString(47)), b)
+}
 
-func Benchmark_RSA4096_94(b *testing.B) { RSA(4096, []byte(generateString(94)), b) }
+func Benchmark_RSA4096_94(b *testing.B) {
+	RSA(4096, []byte(generateString(94)), b)
+}
 
-func Benchmark_RSA4096_188(b *testing.B) { RSA(4096, []byte(generateString(188)), b) }
+func Benchmark_RSA4096_188(b *testing.B) {
+	RSA(4096, []byte(generateString(188)), b)
+}
 
-func Benchmark_RSA2048Encrypt(b *testing.B) { RSAEncrypt(2048, []byte(generateString(188)), b) }
+func Benchmark_RSA2048Encrypt(b *testing.B) {
+	RSAEncrypt(2048, []byte(generateString(188)), b)
+}
 
-func Benchmark_RSA4096Encrypt(b *testing.B) { RSAEncrypt(4096, []byte(generateString(188)), b) }
+func Benchmark_RSA4096Encrypt(b *testing.B) {
+	RSAEncrypt(4096, []byte(generateString(188)), b)
+}
 
-func Benchmark_RSA2048Decrypt(b *testing.B) { RSADecrypt(2048, []byte(generateString(188)), b) }
+func Benchmark_RSA2048Decrypt(b *testing.B) {
+	RSADecrypt(2048, []byte(generateString(188)), b)
+}
 
-func Benchmark_RSA4096Decrypt(b *testing.B) { RSADecrypt(4096, []byte(generateString(188)), b) }
+func Benchmark_RSA4096Decrypt(b *testing.B) {
+	RSADecrypt(4096, []byte(generateString(188)), b)
+}
 
 func RSA(key int, message []byte, b *testing.B) {
 	b.StopTimer()
@@ -181,11 +201,48 @@ func RSADecrypt(key int, message []byte, b *testing.B) {
 	}
 }
 
-func Benchmark_ECIES256(b *testing.B) { ECIESCurveN(elliptic.P256(), b) }
+func Benchmark_ECIES256_47(b *testing.B) {
+	ECIES(elliptic.P256(), []byte(generateString(47)), b)
+}
 
-func Benchmark_ECIES521(b *testing.B) { ECIESCurveN(elliptic.P521(), b) }
+func Benchmark_ECIES256_94(b *testing.B) {
+	ECIES(elliptic.P256(), []byte(generateString(94)), b)
+}
 
-func ECIESCurveN(ell elliptic.Curve, b *testing.B) {
+func Benchmark_ECIES256_188(b *testing.B) {
+	ECIES(elliptic.P256(), []byte(generateString(188)), b)
+}
+
+func Benchmark_ECIES521_47(b *testing.B) {
+	ECIES(elliptic.P521(), []byte(generateString(47)), b)
+}
+
+func Benchmark_ECIES521_94(b *testing.B) {
+	ECIES(elliptic.P521(), []byte(generateString(94)), b)
+}
+
+func Benchmark_ECIES521_188(b *testing.B) {
+	ECIES(elliptic.P521(), []byte(generateString(188)), b)
+}
+
+func Benchmark_ECIES256Encrypt(b *testing.B) {
+	ECIESEncrypt(elliptic.P256(), []byte(generateString(188)), b)
+}
+
+func Benchmark_ECIES521Encrypt(b *testing.B) {
+	ECIESEncrypt(elliptic.P521(), []byte(generateString(188)), b)
+}
+
+func Benchmark_ECIES256Decrypt(b *testing.B) {
+	ECIESDecrypt(elliptic.P256(), []byte(generateString(188)), b)
+}
+
+func Benchmark_ECIES521Decrypt(b *testing.B) {
+	ECIESDecrypt(elliptic.P521(), []byte(generateString(188)), b)
+}
+
+func ECIES(ell elliptic.Curve, message []byte, b *testing.B) {
+	b.StopTimer()
 	prk, err := getKey(ell)
 	if err != nil {
 		b.Fatal(err)
@@ -193,30 +250,81 @@ func ECIESCurveN(ell elliptic.Curve, b *testing.B) {
 	prk2 := ecies.ImportECDSA(prk)
 	puk2 := prk2.PublicKey
 
-	NLongString := func(n int, b *testing.B) {
-		b.StopTimer()
-		for i := 0; i < b.N; i++ {
-			bdata := []byte(generateString(n))
-			//bdata := []byte(calculateHashcode(data))
-			b.ReportAllocs()
-			b.StartTimer()
-			endata, err := ECCEncrypt([]byte(bdata), puk2)
-			if err != nil {
-				b.Fatal(err)
-			}
+	//bdata := message  //без хеширования
+	bdata := []byte(calculateHashcode(string(message)))
 
-			dedata, err := ECCDecrypt(endata, *prk2)
-			if err != nil {
-				b.Fatal(err)
-			}
-			b.StopTimer()
-			result = []byte(hex.EncodeToString(endata))
-			result = dedata
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		endata, err := ECCEncrypt([]byte(bdata), puk2)
+		if err != nil {
+			b.Fatal(err)
 		}
+
+		dedata, err := ECCDecrypt(endata, *prk2)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StopTimer()
+		result = []byte(hex.EncodeToString(endata))
+		result = dedata
+
 	}
-	b.Run("string47", func(b *testing.B) { NLongString(47, b) })
+}
 
-	b.Run("string94", func(b *testing.B) { NLongString(94, b) })
+func ECIESEncrypt(ell elliptic.Curve, message []byte, b *testing.B) {
+	b.StopTimer()
+	prk, err := getKey(ell)
+	if err != nil {
+		b.Fatal(err)
+	}
+	prk2 := ecies.ImportECDSA(prk)
+	puk2 := prk2.PublicKey
 
-	b.Run("string188", func(b *testing.B) { NLongString(188, b) })
+	//bdata := message  //без хеширования
+	bdata := []byte(calculateHashcode(string(message)))
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		endata, err := ECCEncrypt([]byte(bdata), puk2)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		b.StopTimer()
+		result = []byte(hex.EncodeToString(endata))
+
+	}
+}
+
+func ECIESDecrypt(ell elliptic.Curve, message []byte, b *testing.B) {
+	b.StopTimer()
+	prk, err := getKey(ell)
+	if err != nil {
+		b.Fatal(err)
+	}
+	prk2 := ecies.ImportECDSA(prk)
+	puk2 := prk2.PublicKey
+
+	//bdata := message  //без хеширования
+	bdata := []byte(calculateHashcode(string(message)))
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+
+		endata, err := ECCEncrypt([]byte(bdata), puk2)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+		dedata, err := ECCDecrypt(endata, *prk2)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StopTimer()
+		result = []byte(hex.EncodeToString(endata))
+		result = dedata
+
+	}
 }
